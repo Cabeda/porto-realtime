bd4c2195c60c7193cc5c68c5130ed956af501205
José Cabeda <jecabeda@gmail.com>
2026-02-11 18:21:35 +0000

    perf: only render stop markers at zoom level 15+

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Build version - updated automatically during build
 const APP_VERSION = '2.0.0';
-const BUILD_TIMESTAMP = '1770833848717';
+const BUILD_TIMESTAMP = '1770834090073';
 const CACHE_NAME = `porto-realtime-v${APP_VERSION}-${BUILD_TIMESTAMP}`;
 const RUNTIME_CACHE = `porto-realtime-runtime-v${APP_VERSION}-${BUILD_TIMESTAMP}`;
 
